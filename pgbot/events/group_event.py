b9fd1ebaa2472abe56bca957e6996eeb884c8766
Kamil Kozioł <koziol.kamil@protonmail.com>
2021-10-03 22:09:46 +0200

    Dodano msg

--- pgbot/events/group_event.py
+++ pgbot/events/group_event.py
@@ -8,7 +8,7 @@ from pgbot import PGBot
 groups = [894213777527021628, 894213867473887294, 894213914307493890, 894213965930987530, 894213993554640906, 894214014312275998, 894214042527338507]
 
 async def on_component(ctx: ComponentContext):
-    if ctx.origin_message_id != 894226439476482078:
+    if ctx.origin_message_id != 894312902461366312:
         return
 
     member: Member = ctx.guild.get_member(ctx.author_id)
